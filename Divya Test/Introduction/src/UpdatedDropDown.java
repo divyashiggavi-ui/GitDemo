93300be2ede7821870ae54f48de34233ce669e10
divyashiggavi-ui <divya.shiggavi96@gmail.com>
2025-11-16 17:59:35 +0530

    added to develop

--- Divya Test/Introduction/src/UpdatedDropDown.java
+++ Divya Test/Introduction/src/UpdatedDropDown.java
@@ -34,7 +34,7 @@ public class UpdatedDropDown {
 		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
 		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
 		if (driver.findElement(By.id("Div1")).getAttribute("style").contains("1")) {
-			System.out.println("its enabled");
+			System.out.println("its enabled successfully");
 			Assert.assertTrue(true);
 		} else {
 			Assert.assertTrue(false);
